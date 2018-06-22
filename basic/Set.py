5a8676a17dfc803c23666d5581ca62f7d97c5b76
Ryo Iino <iinoryo@team-lab.com>
2018-06-22 19:09:28 +0900

    feat: edit set coding

--- basic/Set.py
+++ basic/Set.py
@@ -1,3 +1,5 @@
+# coding:utf-8
+
 test_set_1 = {'python', '-', 'izm', '.', 'com'}
 print(test_set_1)
 
@@ -5,3 +7,17 @@ print('---------------------------------------')
 
 for i in test_set_1:
     print(i)
+
+# これはディクショナリ
+test_dic = {}
+
+# これはセット
+test_set = {'python'}
+
+# からのセットは「set」を使用する
+empty_set = set()
+
+test_set_1.remove('-')
+test_set_1.discard('.')
+
+print(test_set_1)
