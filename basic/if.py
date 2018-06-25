68b0ebdb48a7b93bd567593d3b3c0cc16f07767c
Ryo Iino <iinoryo@team-lab.com>
2018-06-25 13:33:20 +0900

    fix: Create if.py

---
+++ basic/if.py
@@ -0,0 +1,25 @@
+# encoding:utf-8
+
+# 基本的なif文
+value = 2
+
+if value == 1:
+    print('valueの値は1です')
+elif value == 2:
+    print('valueの値は2です')
+elif value == 3:
+    print('valueの値は3です')
+else:
+    print('該当する値はありません')
+
+# 条件の組み合わせ
+value_1 = 'python'
+value_2 = 'izm'
+
+if value_1 == 'Python':
+    # 何もしない
+    pass
+elif value_1 == 'python' and value_2 == 'izm':
+    print('2番目の条件式がTrue')
+elif value_1 == 'IZM' or value_2 == 'PYTHON':
+    print('3番目の条件式がTrue')
