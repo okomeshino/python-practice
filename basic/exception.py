3c080e3ff04c80816fa72ca166f2350bedbbcc30
Ryo Iino <iinoryo@team-lab.com>
2018-06-25 14:57:41 +0900

    feat: Create exception.py

---
+++ basic/exception.py
@@ -0,0 +1,30 @@
+# encoding:utf-8
+
+import sys
+import traceback
+
+
+def exception_test(value_1, value_2):
+
+    print('=========計算開始=========')
+
+    result = 0
+
+    try:
+        result = value_1 + value_2
+    except:
+        print('計算できませんでした！')
+        raise
+    finally:
+        print('計算終了')
+
+    return result
+
+
+try:
+    print(exception_test(100, 200))
+    print(exception_test(100, '200'))
+except:
+    print('---------------------------')
+    print(traceback.format_exc(sys.exc_info()[2]))
+    print('---------------------------')
