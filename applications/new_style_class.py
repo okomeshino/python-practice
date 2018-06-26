e0dbf5d759f9e934771204247a262e35430cd849
Ryo Iino <iinoryo@team-lab.com>
2018-06-26 12:30:46 +0900

    feat: Create new_style_class.py

---
+++ applications/new_style_class.py
@@ -0,0 +1,22 @@
+# encoding:utf-8
+
+
+# 新クラススタイル
+class NewStyleClassBase(object):
+
+    def test_method(self, msg):
+        print('NewStyleClassBase: {}'.format(msg))
+
+
+# 新スタイルのクラスを継承
+class NewStyleClass(NewStyleClassBase):
+
+    def test_method(self, msg):
+        print('NewStyleClass: {}'.format(msg))
+        super().test_method(msg)
+        # super(NewStyleClass, self).test_method(msg)
+        # NewStyleClassBase.test_method(self, msg)
+
+
+new = NewStyleClass()
+new.test_method('method call')
