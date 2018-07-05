988164f69fc5e0152c44c9bf982143e7cdf7b1ef
Ryo Iino <iinoryo@team-lab.com>
2018-07-05 17:51:48 +0900

    feat: Create config

---
+++ applications/config.py
@@ -0,0 +1,15 @@
+import configparser
+
+inifile = configparser.ConfigParser()
+inifile.read('applications/config.ini', 'UTF-8')
+
+print(inifile.get('settings', 'host'))
+print(inifile.get('settings', 'port'))
+
+print(inifile.get('system', 'os'))
+print(inifile.get('system', 'version'))
+print(inifile.get('system', 'path'))
+
+print(inifile.get('user', 'name'))
+print(inifile.get('user', 'password'))
+print(inifile.get('user', 'mail'))
