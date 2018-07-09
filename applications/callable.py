9d627dfe9625e3000912467d51c5f9fbf223f112
Ryo Iino <iinoryo@team-lab.com>
2018-07-09 16:58:41 +0900

    feat: Create callable.py

---
+++ applications/callable.py
@@ -0,0 +1,19 @@
+# encoding:utf-8
+import sys
+
+
+def func_test():
+    print('function')
+
+
+class ClassTest:
+    pass
+
+
+str_test = 'str'
+
+
+print(callable(sys))
+print(callable(func_test))
+print(callable(ClassTest))
+print(callable(str_test))
