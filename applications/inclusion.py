e15b804cca4e223ec14c242290c964d5e8ab0b0f
Ryo Iino <iinoryo@team-lab.com>
2018-07-09 14:13:25 +0900

    feat: Create inclusion.py

---
+++ applications/inclusion.py
@@ -0,0 +1,51 @@
+# encoding:utf-8
+
+# リストの内包表記
+
+# forの前のiがリストの要素になる
+comp_list = [i for i in range(10)]
+print(comp_list)
+
+print('-----------------------------')
+
+comp_list = [str(i * i) for i in range(10)]
+print(comp_list)
+
+print('-----------------------------')
+
+# ディクショナリの内包表記
+
+comp_dict = {str(i): i * i for i in range(10)}
+print(comp_dict)
+
+print('-----------------------------')
+
+# セットの内包表記
+comp_set = {str(i * i) for i in range(10)}
+print(comp_set)
+
+print('-----------------------------')
+
+# forのネスト（ネストも可能）
+comp_list = [i * ii for i in range(1, 10) for ii in range(1, 10)]
+print(comp_list)
+
+print('-----------------------------')
+
+# 以下と同じ
+# comp_list = []
+# for i in range(1, 10):
+#     for ii in range(1, 10):
+#         comp_list.append(i * ii)
+#
+# print(comp_list)
+
+# ifとの併用
+comp_list = [i for i in range(10) if i % 2 == 1]
+print(comp_list)
+
+print('-----------------------------')
+
+# タプル内包表記（は無い）
+gen = (i for i in range(10))
+print(gen)
