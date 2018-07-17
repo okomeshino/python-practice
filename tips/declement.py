1b4b077c50a91f53de06b10937c39b8ff5d7742a
Ryo Iino <iinoryo@team-lab.com>
2018-07-17 19:03:37 +0900

    feat: Create inclement.py and declement.py

---
+++ tips/declement.py
@@ -0,0 +1,7 @@
+# encoding:utf-8
+
+test_num = 100
+print(test_num)
+
+test_num -= 1
+print(test_num)
---
+++ tips/inclement.py
@@ -0,0 +1,9 @@
+# encoding:utf-8
+test_num = 100
+print(test_num)
+
+test_num += 1
+print(test_num)
+
+# pythonではエラーになる
+# test_num++
