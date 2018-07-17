2890b8f9185401daf44a34943f617087f0b8c310
Ryo Iino <iinoryo@team-lab.com>
2018-07-17 12:35:40 +0900

    feat: Create dir.py

---
+++ applications/dir.py
@@ -0,0 +1,14 @@
+# encoding:utf-8
+
+print('-----------------------------------')
+print(dir())
+
+python_dir = 'python-izm'
+
+print('-----------------------------------')
+print(dir())
+
+print('-----------------------------------')
+import sys
+for one in dir(sys):
+    print(one)
