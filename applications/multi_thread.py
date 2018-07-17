c1e823169428ab3531cbdb15f01c9c78faefd657
Ryo Iino <iinoryo@team-lab.com>
2018-07-17 17:54:07 +0900

    feat: Create multi_thread.py

---
+++ applications/multi_thread.py
@@ -0,0 +1,26 @@
+# encoding:utf-8
+import threading
+import time
+import datetime
+
+
+class TestThread(threading.Thread):
+
+    def run(self):
+        print('=== start sub thread ===')
+        for i in range(5):
+            time.sleep(5)
+            print(' sub thread : ' + str(datetime.datetime.today()))
+        print('=== end sub thread ===')
+
+
+th = TestThread()
+th.start()
+
+time.sleep(1)
+
+print('=== start main thread ===')
+for i in range(5):
+    time.sleep(10)
+    print('main thread : ' + str(datetime.datetime.today()))
+print('=== end main thread ===')
